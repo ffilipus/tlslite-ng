2e52969f49e294c430331ae4db58943d8d1399d6
Filip Goldefus <fgoldefu@redhat.com>
2016-05-17 21:02:40 +0200

    Jython2.7 bytearray bug fix.

--- tlslite/utils/compat.py
+++ tlslite/utils/compat.py
@@ -8,6 +8,7 @@ import os
 import math
 import binascii
 import ecdsa
+import platform
 
 if sys.version_info >= (3,0):
 
@@ -55,7 +56,7 @@ if sys.version_info >= (3,0):
 else:
     # Python 2.6 requires strings instead of bytearrays in a couple places,
     # so we define this function so it does the conversion if needed.
-    if sys.version_info < (2,7):
+    if sys.version_info < (2,7) or platform.system() == 'Java':
         def compat26Str(x): return str(x)
     else:
         def compat26Str(x): return x
